b052e27727f0ace35f9acb6983c3d7909498a697
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-12-06 20:29:14 +0100

    init project template

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  images: {
+    deviceSizes: [360, 480, 768, 1024, 1280, 1366, 1440, 1920, 2048],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
